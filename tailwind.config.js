c4411504011a6688c7b2817d394ebf059ea73594
sami <sami.anki1316@gmail.com>
2024-11-08 20:48:04 +0100

    feat: add tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+import tailwindScrollBar from "tailwind-scrollbar";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontfamily: {
+        sans: ["Inter var", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [tailwindScrollBar],
 };
