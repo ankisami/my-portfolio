7f4e5f4142be7ab431e534138deb37a78ff2b5f6
sami <sami.anki1316@gmail.com>
2024-11-13 00:39:51 +0100

    change: fixe font size

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -9,8 +9,10 @@ const ExperienceCard = ({
 }: Experience) => {
   return (
     <div className=" bg-zinc-800 p-5 rounded-xl flex flex-col justify-between w-[315px] gap-3">
-      <p>{title}</p>
-      <p className="text-zinc-400  italic">~ {company}</p>
+      <div>
+        <p>{title}</p>
+        <p className="text-zinc-400 text-sm italic">~ {company}</p>
+      </div>
 
       <div className="flex flex-wrap items-center gap-1">
         {tags?.map((tag) => (
@@ -23,11 +25,13 @@ const ExperienceCard = ({
         ))}
       </div>
 
-      {description && <p>{description}</p>}
+      {description && (
+        <p className="h-6 text-xs text-zinc-400">{description}</p>
+      )}
 
       <div className="flex  items-center italic text-zinc-400 text-sm gap-3">
         <span className="material-symbols-rounded">calendar_month</span>
-        <p className="text-sm">{duration}</p>
+        <p className="text-xs">{duration}</p>
       </div>
     </div>
   );
